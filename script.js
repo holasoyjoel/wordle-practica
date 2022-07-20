5ee09143305f1d8ee0e7792a9367308339e17ed4
Correa Joel <joelcorrea.alejandro123@gmail.com>
2022-07-20 04:57:01 -0300

    finalizado de escritura letra, y autofocus

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ const columnas = 4;
 
 const fieldsets = document.querySelectorAll('fieldset');
 
-let oportunidades = 6;
+let oportunidades = 5;
 let filaPosicionada = 0;
 let columnaPosicionada = 0;
 
@@ -40,23 +40,32 @@ const respuestas = [
 const comenzarJuego = () => {
     seleccionarColumna();
     eventoLetra();
+    console.log(oportunidades);
 };
 
 const eventoLetra = () => {
     window.addEventListener('keyup' , (event) => {
-        if(event.code !== 'Space'){
-            
-            if(event.key == 'Enter' && oportunidades > 0 && respuestas[filaPosicionada].length === 5){
-                console.log('siguiente');
+        if(event.code !== 'Space'){            
+            if(event.key == 'Enter' && oportunidades > 1 && respuestas[filaPosicionada].length === 5){
+                fieldsets[filaPosicionada].querySelectorAll('input')[4].style.border = '0px solid white';
+                oportunidades -= 1;
+                filaPosicionada += 1;
+                columnaPosicionada = 0;
+                seleccionarColumna();
+            }
+            else if(event.key == 'Enter' && oportunidades == 1 && respuestas[filaPosicionada].length === 5){
+                fieldsets[filaPosicionada].querySelectorAll('input')[4].style.border = '0px solid white';
             }
             else{
-                if(event.key === 'Backspace'){
-                    escribirLetra(' ');
+                if(String(event.key).match('[A-Za-z]')){
+                    if(event.key === 'Backspace'){
+                        escribirLetra(' ');
+                    }
+                    else{
+                        escribirLetra(event.key)
+                    }
+                    seleccionarColumna();
                 }
-                else{
-                    escribirLetra(event.key)
-                }
-                seleccionarColumna();
             }
         }
     })
@@ -69,63 +78,56 @@ const seleccionarColumna = () => {
         
         // despintar columna anterio y siguiente
         if(columnaPosicionada - 1 >= 0){
-            fieldsets[filaPosicionada].querySelectorAll('input')[columnaPosicionada - 1].style.border = '3px solid white';
+            fieldsets[filaPosicionada].querySelectorAll('input')[columnaPosicionada - 1].style.border = '0px solid white';
         }
         if(columnaPosicionada + 1 < 5){
-            fieldsets[filaPosicionada].querySelectorAll('input')[columnaPosicionada + 1].style.border = '3px solid white';
+            fieldsets[filaPosicionada].querySelectorAll('input')[columnaPosicionada + 1].style.border = '0px solid white';
         }
     }
 }
 
 const escribirLetra = (letra) => {
     let cantidadLetras = respuestas[filaPosicionada].length;
-    
-    // si hay al menos 5 letras y la letra es un caracter
+    // entre 0 y 5 letras , ademas que letra tenga un caracter (para evitar enter, cntrl, etc)
     if((cantidadLetras < 5 && cantidadLetras >= 0) && letra.length === 1){
-    
-        // si la letra machea o es un espacio(delete)
-        if(String(letra).match('[A-Za-z]') || letra === ' '){
-            
-            // si la letra no es para borrar
-            if(letra !== ' '){                
-                
-                // escribo la letra
+            // si se apreto algo distinto a delete
+            if(letra !== ' '){                                
                 fieldsets[filaPosicionada].querySelectorAll('input')[columnaPosicionada].value = letra;
-
                 if(cantidadLetras < 5){
                     if(columnaPosicionada < 4){
-                        columnaPosicionada += 1;
+                        columnaPosicionada += 1; // evitando desbordamiento para autofocus de cuadro
                     }
                     respuestas[filaPosicionada].push(letra);
                 }
-            }
-            
+            }            
+            // si apreto delete pero hay letra escritas
             else if(letra === ' ' && cantidadLetras > 0){
                 fieldsets[filaPosicionada].querySelectorAll('input')[columnaPosicionada].value = letra;
                 columnaPosicionada -= 1;
                 respuestas[filaPosicionada].pop();
-                if(columnaPosicionada == -1){ columnaPosicionada = 0}
+                if(columnaPosicionada == -1){ 
+                    columnaPosicionada = 0 // evitando desbordamiento para autofocus de cuadro
+                }
             }
+            // si apreto delete para borrar primera letra
             else if(letra === ' ' && cantidadLetras === 0){
                 fieldsets[filaPosicionada].querySelectorAll('input')[columnaPosicionada].value = letra;
                 respuestas[filaPosicionada].pop();
-            }
-        }
+            }        
     }
-    // borrando ultima letra
-    else if(cantidadLetras === 5 && letra !== ' '){
+    // cambiando ultima letra
+    else if(cantidadLetras === 5 && letra !== ' ' && letra.length == 1){
         fieldsets[filaPosicionada].querySelectorAll('input')[4].value = letra;
         respuestas[filaPosicionada].pop();
         respuestas[filaPosicionada].push(letra);
     }
-    // cambiando ultima letra
+    // borrando ultima letra
     else if(cantidadLetras === 5 && cantidadLetras >= 0 && letra === ' '){
         fieldsets[filaPosicionada].querySelectorAll('input')[4].value = letra;
         respuestas[filaPosicionada].pop();
         respuestas[filaPosicionada].pop();
         columnaPosicionada = 3;
     }
-    console.log(respuestas);
 }
 
 window.onload = () => {
